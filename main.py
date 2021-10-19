9addb7802b6c74f8402da76d8da1ef4e5f5c982c
Gerard Hager <ghager93@gmail.com>
2021-10-19 14:54:32 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+
+from kivy.app import App
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.boxlayout import BoxLayout
+from kivy.factory import Factory
+from kivy.uix.filechooser import FileChooserListView
+from kivy.properties import ObjectProperty, BooleanProperty
+from kivy.uix.popup import Popup
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
+                                 RecycleBoxLayout):
+    ''' Adds selection and focus behaviour to the view. '''
+
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+
+class Explorer(FileChooserListView):
+    dirselect = True
+
+    def on_selection(self, *args):
+        print(self.selection)
+        App.get_running_app().root.ids.rv.update_data(self.selection)
+
+class DirList(RecycleView):
+    def __init__(self, **kwargs):
+        super(DirList, self).__init__(**kwargs)
+        self.data = [{'text': 'hello'}]
+
+    def update_data(self, data):
+        self.data = [{'text':str(d)} for d in os.listdir(data[0])]
+
+class Root(BoxLayout):
+    filechooser = Explorer()
+
+
+class Editor(App):
+    def build(self):
+        self.root.ids.filechooser.bind(on_selection=self.root.ids.rv.update_data)
+
+if __name__ == '__main__':
+    Editor().run()
